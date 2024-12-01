cbcf87518a13c490349522536abca49a7ed6b011
mkireva <mariya@kireva.de>
2024-12-01 11:19:45 +0100

    fix: icons

--- src/app/sheets/components/TabIcon.tsx
+++ src/app/sheets/components/TabIcon.tsx
@@ -5,8 +5,8 @@ interface TabIconProps {
 
 export function TabIcon({ icon: Icon, isSelected }: TabIconProps) {
   return (
-    <div className={`w-6 h-6 ${isSelected ? "text-foreground" : ""}`}>
-      <Icon size={24} className="w-full h-full" />
+    <div className={`w-6 h-6 md:w-6 md:h-6 ${isSelected ? "text-foreground" : ""}`}>
+      <Icon size={20} className="w-full h-full md:w-6 md:h-6" />
     </div>
   );
 }
