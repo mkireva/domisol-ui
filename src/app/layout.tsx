b1cee4a9bc81202a260c2778370160599ec91791
mkireva <mariya@kireva.de>
2025-05-16 13:03:12 +0200

    fix: design wip 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
